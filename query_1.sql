de1564eb7da36734a722e48cf032c61ff6ae1327
Bohdan Bokariev <b.bokariev@gmail.com>
2023-09-13 23:27:11 +0300

    Resolved problems with students name and subj name

--- query_1.sql
+++ query_1.sql
@@ -1,11 +1,15 @@
 SELECT
-  student_id,
-  subject_id ,
-  AVG(mark) AS avg_mark
+  stu.name AS student_name,
+  s.name AS subject_name,
+  AVG(m.mark) AS avg_mark
 FROM
-  marks
+  marks m
+JOIN
+  students stu ON m.student_id = stu.id
+JOIN
+  subjects s ON m.subject_id = s.id
 GROUP BY
-  student_id
+  stu.name, s.name
 ORDER BY
   avg_mark DESC
 LIMIT
--- query_2.sql
+++ query_2.sql
@@ -1,13 +1,17 @@
 SELECT
-  student_id,
-  subject_id,
-  AVG(mark) AS avg_mark
+  stu.name AS student_name,
+  s.name AS subject_name,
+  AVG(m.mark) AS avg_mark
 FROM
-  marks
+  marks m
+JOIN
+  students stu ON m.student_id = stu.id
+JOIN
+  subjects s ON m.subject_id = s.id
 WHERE
   subject_id = 5
 GROUP BY
-  student_id
+  stu.name, s.name
 ORDER BY
   avg_mark DESC
 LIMIT
